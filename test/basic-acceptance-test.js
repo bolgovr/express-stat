1095b635139300c2b734fede33a320770c2e5d6d
Roman <bolgovr@gmail.com>
2012-04-12 22:22:36 +0300

    add test for basic express app

--- test/basic-acceptance-test.js
+++ test/basic-acceptance-test.js
@@ -1,5 +1,26 @@
 var assert = require('assert');
+var Zombie = require('zombie');
+describe('express', function () {
+  var app, browser;
 
-describe('module should return acceptable middleware for express', function () {
-  it('just dummy test');
+  beforeEach(function (done) {
+    app = require('./fixtures/express-app.js');
+    app.listen(8083);
+    browser = new Zombie();
+    done();
+  });
+
+  afterEach(function (done) {
+    app.close();
+    done();
+  });
+
+  it('application should run', function (done) {
+    browser.visit('http://localhost:8083/', function (error, browser) {
+      assert.equal(browser.success, true);
+      assert.equal(browser.response[0], 200); //http status code
+      assert.equal(browser.response[2], "ok");
+      done();
+    });
+  });
 });
